f4f11d66cd4e1c520ee2e10eb04b6e91da5c92ae
xzj <xzj007@gmail.com>
2014-07-27 01:03:24 +0800

    Added: mysql db driver dependency

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [korma "0.3.0"]
+                 [mysql/mysql-connector-java "5.1.25"]
                  ])
