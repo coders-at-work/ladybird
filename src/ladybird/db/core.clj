98721e2ddfba2dfc8b88d85d4d42d97052e644c3
xzj <xzj007@gmail.com>
2014-07-29 23:39:18 +0800

    Changed: added doc-string in ladybird.db.core

--- src/ladybird/db/core.clj
+++ src/ladybird/db/core.clj
@@ -14,35 +14,41 @@
 (defn- add-to-conn-map [name value]
        (reset! conn-map (assoc @conn-map name value)))
 
-(defn get-conn [name]
+(defn get-conn
+  "Get the db connection related objects specified by the name."
+  [name]
   (@conn-map name))
 
-(defn get-db-conn [name]
+(defn get-db-conn
+  "Get the jdbc connection specified by the name."
+  [name]
   (get-in @conn-map [name :db-conn]))
 
 ;; current connection
 (def ^:dynamic *cur-conn-name* nil)
 
 (defn cur-conn-name
-  "Get the current db connection name. If *cur-conn-name* has been bound, return it; otherwise, return the main db connection name."
+  "Get the current db connection name. If *cur-conn-name* has been bound, return it; otherwise, return the main db connection name(:main)."
   []
   (or *cur-conn-name* :main))
 
-(defn get-cur-conn []
+(defn get-cur-conn
+  "Get the current db connection related objects."
+  []
   (get-conn (cur-conn-name)))
 
-(defn get-cur-db-conn []
+(defn get-cur-db-conn
+  "Get the current jdbc connection."
+  []
   (get-db-conn (cur-conn-name)))
 
-;(def-bindable cur-conn nil)
-
-#_(defmacro with-cur-conn-as [name & body]
-  `(with-cur-conn (get-conn ~name)
-                  ~@body))
-
 ;; db preparation
-(defn init-db [name db-def-map]
+(defn init-db
+  "Init a db connection and name it."
+  [name db-def-map]
   (->> (dbk/init-db db-def-map) (merge {:original-conn-def db-def-map}) (add-to-conn-map name)))
 
-(defn init-main-db [db-def-map]
+(defn init-main-db
+  "Init the main db connection(the db connection named :main)."
+  [db-def-map]
   (init-db :main db-def-map))
