c41b4abb01757ab98589ff159a9538557e34eedf
xzj <xzj007@gmail.com>
2014-08-05 02:30:27 +0800

    Changed: added ladybird.util.string/hyphen-to-underscore and ladybird.util.string/underscore-to-hyphen

--- src/ladybird/util/string.clj
+++ src/ladybird/util/string.clj
@@ -1,2 +1,9 @@
-(ns ladybird.util.string)
+(ns ladybird.util.string
+    (:require [clojure.string :as string])
+    )
 
+(defn hyphen-to-underscore [s]
+  (string/replace s "-" "_"))
+
+(defn underscore-to-hyphen [s]
+  (string/replace s "_" "-"))
