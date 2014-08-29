948db4da536e8717a7ed4e5c6ac4edf34c328c30
xzj <xzj007@gmail.com>
2014-08-29 19:30:31 +0800

    Added: ladybird.data.converter-core, ladybird.data.db-converter

---
+++ src/ladybird/data/converter_core.clj
@@ -0,0 +1,43 @@
+(ns ladybird.data.converter-core)
+
+(defn convert-fn [c c-type] {:pre [(#{:in :out} c-type)]}
+  (c-type c))
+
+(defn in-fn [c] (convert-fn c :in))
+
+(defn out-fn [c] (convert-fn c :out))
+
+(defn value-converter [keyvals]
+  {:pre [(not (empty? keyvals)) (even? (count keyvals)) (every? #(not (nil? %)) keyvals)]}
+  {:in (apply hash-map (reverse keyvals)) :out (apply hash-map keyvals)})
+
+(defn composite-converter [conv & convs]
+  (let [convs (apply list conv convs)
+        r-convs (reverse convs)
+        composite-conv-fn (fn [in-out convs] (apply comp (map in-out convs)))]
+    {:in (composite-conv-fn :in convs) :out (composite-conv-fn :out r-convs)}))
+
+;; TODO should change to [pred-in fin pred-out fount]
+(defn when-not-converter
+  "converter based upon the logical truth of a predicate. If it is true, the object will not be converted.
+   If it is false, in or out function will be called to convert the object.
+   Args:
+   Return:
+   a converted obj"
+  ([pred f]
+   (when-not-converter pred f f))
+  ([pred fin fout]
+   (let [convert-when-not (fn [f] #(if (pred %) % (f %)))]
+     {:in (convert-when-not fin) :out (convert-when-not fout)})))
+
+(defn when-converter
+  ([pred f]
+   (when-converter pred f f))
+  ([pred fin fout]
+   (when-not-converter (complement pred) fin fout)))
+
+(defn nullable-converter
+  ([f]
+   (nullable-converter f f))
+  ([fin fout]
+   (when-not-converter nil? fin fout)))
---
+++ src/ladybird/data/db_converter.clj
@@ -0,0 +1,28 @@
+(ns ladybird.data.db-converter
+    (:use data.converter-core
+          [util.str-utils :only (datetime-str-to-date)])
+    (:import (java.util Date Calendar)))
+
+(defn- truncate-time [^java.util.Date d]
+       (let [c (Calendar/getInstance)]
+         (doto c
+               (.setTime d)
+               (.set Calendar/HOUR_OF_DAY 0)
+               (.set Calendar/MINUTE 0)
+               (.set Calendar/SECOND 0))
+         (.getTime c)))
+
+(def BOOL (value-converter [true "T" false "F"]))
+
+(def DATETIME (nullable-converter #(java.util.Date. (.getTime %)) #(-> (.getTime %) java.sql.Timestamp. (doto (.setNanos 0)))))
+
+(def DATE (composite-converter {:in identity :out truncate-time} DATETIME))
+
+(def DECIMAL (nullable-converter bigdec))
+
+(def STR->DATETIME (composite-converter (when-converter string? datetime-str-to-date) DATETIME))
+
+;; TODO should be STRING-OUT-INT-IN
+(def INT->STRING (when-converter integer? str))
+
+;; datetime, encryption, simplfied/traditional chinese
