a455cb408f98815f1ba338a8a2259412a3c0c677
xzj <xzj007@gmail.com>
2014-08-29 22:59:22 +0800

    Changed: added some date-string convertion functions

--- src/ladybird/util/string.clj
+++ src/ladybird/util/string.clj
@@ -1,6 +1,7 @@
 (ns ladybird.util.string
     (:require [clojure.string :as string])
-    )
+    (:import [java.util Date]
+             [java.text SimpleDateFormat]))
 
 (defn hyphen-to-underscore [s]
   (string/replace s "-" "_"))
@@ -19,4 +20,18 @@
 
 (defn db-case-to-clj-case [s]
   (-> s string/lower-case underscore-to-hyphen))
+
+;; convert between Date and String
+(def datetime-format-str "yyyy-MM-dd HH:mm:ss")
+(def ^{:private true} datetime-format (SimpleDateFormat. datetime-format-str))
+(def date-format-str "yyyy-MM-dd")
+(def ^{:private true} date-format (SimpleDateFormat. date-format-str))
+(def time-format-str "HH:mm:ss")
+(def ^{:private true} time-format (SimpleDateFormat. time-format-str))
+
+(defn date-to-datetime-str [^Date d]
+  (.format datetime-format d))
+
+(defn datetime-str-to-date [str]
+  (.parse datetime-format str))
 
