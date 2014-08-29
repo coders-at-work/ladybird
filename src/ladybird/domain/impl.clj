11fa9cbab375d0a702f9a47d2c9b7b5051b21dd4
xzj <xzj007@gmail.com>
2014-08-29 23:02:24 +0800

    Changed: supports field converters to convert records

--- src/ladybird/domain/impl.clj
+++ src/ladybird/domain/impl.clj
@@ -24,6 +24,22 @@
         ]
     ret))
 
+;; convert
+(defn convert-value [c-type converters k v]
+  (if-let [c (get-in converters [k c-type])]
+          (c v)
+          v))
+
+(defn convert-record-in [{:keys [converters] :as spec} rec]
+  (reduce (fn [m [k v]]
+              (assoc m k (convert-value :in converters k v)))
+          {} rec))
+
+(defn convert-record-out [{:keys [converters] :as spec} rec]
+  (reduce (fn [m [k v]]
+              (assoc m (domain-field-to-db-field k) (convert-value :out converters k v)))
+          {} rec))
+
 ;; crud
 (defn query
   ""
@@ -33,7 +49,7 @@
   ([table {:keys [fields converters aggregate join] :as query-spec} condition]
          (let [spec (create-select-spec query-spec)
                ]
-     (dml/select table condition spec))
+     (->> (dml/select table condition spec) (map #(convert-record-in query-spec %))))
    #_(->> (select table (condition-to-where condition) spec)
         (map #(convert-datum-in % spec)))) 
   )
@@ -48,7 +64,10 @@
   ([table data]
    (add! table {} data))
   ([table {:keys [converters] :as spec} & data]
-   (let [tr-fn #(reduce
+   (dml/insert! table
+                (map #(convert-record-out spec %) data)
+                spec)
+   #_(let [tr-fn #(reduce
                   (fn [ret [k v]] (assoc ret (domain-field-to-db-field k) v))
                   {} %)]
      (dml/insert! table (map tr-fn data) spec))
