2778e99302984f569a5cd96d9a30646f1262de92
xzj <xzj007@gmail.com>
2014-09-01 15:56:39 +0800

    Added: ladybird.data.cond

---
+++ src/ladybird/data/cond.clj
@@ -0,0 +1,65 @@
+(ns ladybird.data.cond
+    (:use [clojure.walk :only (postwalk)]))
+
+(defn- convert-op-value[op value]
+       (condp = op
+         '<> ['!= value]
+         'contain ['like (str "%" value "%")]
+         'start-with ['like (str value "%")]
+         'end-with ['like (str "%" value)]
+         'is-null ['nil?]
+         [op value]))
+
+(defn- pred [[op field value]]
+       (let [[op value] (convert-op-value op value)]
+         (if (nil? value)
+           `(list '~op ~field)
+           `(list '~op ~field ~value))))
+
+(defn- logical [[op & args]]
+  `(list '~op ~@args))
+
+(defn- pred? [x]
+  (and (seq? x) (#{'= '!= '< '> '<= '>= 'like 'nil? 'in '<> 'contain 'start-with 'end-with 'is-null} (first x))))
+
+(defn- logical? [x]
+  (and (seq? x) (#{'and 'or 'not} (first x))))
+
+;; build-in operators used by condition
+;;  =    
+;;  !=   
+;;  <    
+;;  >    
+;;  <=   
+;;  >=   
+;;  and  
+;;  or   
+;;  not  
+;;  like 
+;;  nil? 
+;;  in   
+;; we add the following operator for convenience
+;;  <>
+;;  contain
+;;  start-with
+;;  end-with
+;;  is-null
+(defmacro make
+  "generate condition which is a list made of build-in operators and values, ex. '(= :id 1)
+   lower adapter of sql abstraction library must transalte this condition to where clause"
+  [conds]
+  (postwalk #(cond (pred? %) (pred %)
+                   (logical? %) (logical %)
+                   :default %)
+            conds))
+
+(def RAW :<raw>)
+
+(defn raw
+  "construct a list which means a raw string in a sql statement"
+  [str]
+  (list RAW str))
+
+(defn raw? [x]
+  (and (list? x)
+       (= RAW (first x))))
