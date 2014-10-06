0355a61a8e4594e6115689b1e56cdd805d36a61b
xzj <xzj007@gmail.com>
2014-10-07 02:35:40 +0800

    Changed: completed ladybird.svc.core/check-and-bind

--- src/ladybird/svc/core.clj
+++ src/ladybird/svc/core.clj
@@ -8,29 +8,47 @@
   (let [body-form `(do ~@body)]
     (-> meta (dissoc :body) (assoc :body-form body-form))))
 
-(defn- construct-check-and-bind-body [specs-m body-form]
-       (let [{:keys [validate vars-m] :as m} (reduce (fn [ret [var-sym var-spec]]
-                                                         (-> (update-in ret [:validate] assoc `'~var-sym var-spec)
-                                                             (update-in [:vars-m] assoc `'~var-sym var-sym)
-                                                             )
-                                                         )
-                                                     {} specs-m)
-             ]
-         `(do
-            (-> ((v/m-validator ~validate) ~vars-m) v/check-validate-result)
-            ~body-form)
-         ;m
-         )
-       )
+(defn- parse-check-and-bind-specs [specs]
+       (reduce (fn [ret [var-sym var-spec]]
+                   (let [contains-converter? (and (vector? var-spec) (= :to (-> var-spec drop-last last)))
+                         validate-spec (if contains-converter? (-> (drop-last 2 var-spec) vec) var-spec)
+                         converter (when contains-converter? (last var-spec))
+                         ret (if (and (vector? validate-spec) (empty? validate-spec))
+                               ret
+                               (-> (update-in ret [:validate] assoc `'~var-sym validate-spec)
+                                   (update-in [:vars-m] assoc `'~var-sym var-sym)))
+                         ]
+                     (if converter
+                       (update-in ret [:converters] #(let [converters (if %1 %1 [])]
+                                                       (conj converters %2 %3))
+                                  var-sym `(~converter ~var-sym))
+                       ret)))
+               {} specs))
 
-(defn check-and-bind [{:keys [options body-form] :as meta}]
+(defn- construct-check-and-bind-body [specs body-form]
+       (let [{:keys [validate converters vars-m] :as m} (parse-check-and-bind-specs (partition 2 specs))
+             body-form (if converters `(let ~converters ~body-form) body-form)
+             ]
+         (if validate
+           `(do
+              (-> ((v/m-validator ~validate) ~vars-m) v/check-validate-result)
+              ~body-form)
+           body-form)))
+
+(defn check-and-bind
+  "
+  Specs should be like: [a [not-nil is-boolean] b not-nil c [:to inc] d [not-nil :to dec]]
+  "
+  [{:keys [options body-form] :as meta}]
   (let [{:keys [check-and-bind]} options]
     (if check-and-bind
       (assoc meta :body-form (construct-check-and-bind-body check-and-bind body-form))
       meta)))
 
 (defn catch-forms [meta]
-  (let [catch-forms `((catch Exception ~'e (log/error (get-stack-trace-str ~'e)) (ex-info "Oops!" {:ex-type :other-exception})))]
+  (let [catch-forms `((catch Exception ~'e
+                             (log/error (clojure.string/join "\n" [(type ~'e) (get-stack-trace-str ~'e)]))
+                             (ex-info "Oops!" {:ex-type :other-exception})))]
     (assoc meta :catch-forms catch-forms)))
 
 (defn gen-defn-with-catch-point [{:keys [svc doc-string prototype body-form catch-forms] :as meta}]
