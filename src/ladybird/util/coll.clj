024e6f42c2a0b7180ef2546549481780b21d95dc
xzj <xzj007@gmail.com>
2014-10-09 18:26:04 +0800

    Added: ladybird.util.coll

---
+++ src/ladybird/util/coll.clj
@@ -0,0 +1,16 @@
+(ns ladybird.util.coll)
+
+(defn consv
+  "
+  Returns a new vector where x is the first element and seq is the rest.
+  "
+  [x seq]
+  (vec (cons x seq)))
+
+
+(defn concatv
+  "
+  Returns a vector representing the concatenation of the elements in the supplied colls.
+  "
+  [& args]
+  (vec (apply concat args)))
