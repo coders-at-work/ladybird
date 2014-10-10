4580a595dbf8210bbe3189419702e88d683919d1
xzj <xzj007@gmail.com>
2014-10-10 23:42:57 +0800

    Changed: added ladybird.util.symbol/meta-flag-symbol

--- src/clj/ladybird/util/symbol.clj
+++ src/clj/ladybird/util/symbol.clj
@@ -7,3 +7,14 @@
       (str-symbol \"a\" :b 'cd)   ;;=> abcd"
   [x & ys]
   (->> (cons x ys) (map name) (apply str) symbol))
+
+(defn meta-flag-symbol
+  "
+  Creates a symbole named by x with the given keywords being set to true in its metadata.
+
+  Example:
+      (meta-flag-symbol \"a\" :dynamic :private)
+      (meta *1) -> {:dynamic true :private true}
+  "
+  [x kw & kws]
+  (->> (repeat true) (zipmap (cons kw kws)) (with-meta (-> x name symbol))))
