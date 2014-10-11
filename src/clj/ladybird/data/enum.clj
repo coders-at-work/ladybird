404c1d5733c4222f0c2c9c34be286f27057f8db7
xzj <xzj007@gmail.com>
2014-10-11 15:18:02 +0800

    Changed: added ladybird.data.enum/defenum-for-keys

--- src/clj/ladybird/data/enum.clj
+++ src/clj/ladybird/data/enum.clj
@@ -2,18 +2,22 @@
     (:use ladybird.data.converter-core
           [ladybird.data.order :only (make-order)]
           [ladybird.util.symbol :only (str-symbol)]
-          [ladybird.util.string :only (qualify-name)])
+          [ladybird.util.string :only (qualify-name)]
+          [ladybird.util.coll :only (mapcatv)])
     (:require [ladybird.data.validate-core :as v]
               [ladybird.data.build-in-validator :as b]))
 
-(defmacro defenum [name k1 v1 & kvs]
-  (let [kvs (apply vector k1 v1 kvs)
-        validator (str-symbol "enum:" name)
-        i18n-msg-key (keyword (qualify-name name))
+(defn enum-body [name kvs]
+  (let [validator (str-symbol "enum:" name)
+        i18n-msg-key (keyword (qualify-name validator))
         ]
     `(do
        (def ~name (value-converter ~kvs))
        (def ~validator (b/enum-of ~name ~i18n-msg-key)))))
+
+(defmacro defenum [name k1 v1 & kvs]
+  (let [kvs (apply vector k1 v1 kvs)]
+    (enum-body name kvs)))
 
 (defn- ordered-kvs-to-order-es [ordered-kvs]
        (let [parts (partition-by vector? ordered-kvs)]
@@ -30,3 +34,10 @@
     `(do
        (defenum ~name ~@kvs)
        (def ~name (->> ~(ordered-kvs-to-order-es ordered-kvs) make-order (merge ~name))))))
+
+(defmacro defenum-for-keys 
+  ([name keys]
+   `(defenum-for-keys ~name name ~keys))
+  ([name str-fn keys]
+   `(let [kvs# (mapcatv list ~keys (map ~str-fn ~keys))]
+      (eval (enum-body '~name kvs#)))))
