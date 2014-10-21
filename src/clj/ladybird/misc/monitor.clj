3ff945f985a0ee5f9dca9da18fd4445f6e14f637
xzj <xzj007@gmail.com>
2014-10-21 23:15:24 +0800

    Added: ladybird.misc.monitor, ladybird.svc.generate, ladybird.svc.build-in-svc

---
+++ src/clj/ladybird/misc/monitor.clj
@@ -0,0 +1,122 @@
+(ns ladybird.misc.monitor
+    (:use [ladybird.util.core :only (def-bindable)]
+          [ladybird.misc.conf :only (def-switcher)])
+    (:require [ladybird.util.symbol :as sym]
+              [ladybird.util.coll :as coll]
+              [clojure.tools.logging :as log]))
+
+;; We usually need to monitor the execution of our programs at some time. Normally we accomplish this task with logging(or something similar, like pritnln etc.) directly. But the model of logging doesn't match monitoring perfectly. The various levels of logging(trace, debug, info, warn, error) don't have so much business with monitoring. And the logging statements spreading in our codes make them in a mess.
+;;
+;; So let's think about what we need when we monitor. The one of the two things we most likely need when monitoring is to monitor the execution path of the code and the arguments' value passed to the functions on the path when errors occured. The other thing is to monitor the elapsed time of some portions of the execution path when performance problems occured.
+;;
+;; Maybe the best way to help programers to code monitoring is automatically generating code to push the execution path information in a stack and to calculate the elapsed time of the execution when defining a function. And the automatically genenrating code also includes a catch point to catch all exceptions thrown in the function and then to log the infomation. This can be implemented via macro.
+;;
+;; This namespace provides infrastructure to recod info in a thread and provides macros to wrap this functionality. Then programmers can accomplish code generation metioned above based on it.
+
+
+
+;; monitor config
+(def-switcher root-monitor-cfg :exec-path false true)
+
+(def ^:private monitor-cfg (atom nil))
+
+(defn set-monitor-cfg!
+  "
+  Notes: Setting a value of a key to false doesn't mean to disable monitoring on that level, just means falling through to its' parent level, up to root.
+  "
+  ([ns-sym val] {:pre [(contains? #{:time :exec-path false} val)]} 
+   (swap! monitor-cfg assoc ns-sym val))
+  ;; TODO: change key from vector to qualified symbol
+  ([ns-sym var-sym val] {:pre [(contains? #{:time :exec-path false} val)]} 
+   (swap! monitor-cfg assoc [ns-sym var-sym] val)))
+
+(defn get-monitor-cfg
+  ([ns-sym]
+   (or (get-in @monitor-cfg [ns-sym])
+       @root-monitor-cfg))
+  ([ns-sym var-sym]
+   (or (get-in @monitor-cfg [[ns-sym var-sym]])
+       (get-monitor-cfg ns-sym))))
+
+
+;; monitor functionality
+(def-bindable mon-info nil)
+
+(defn call-stack-info []
+  (when-let [mi (mon-info)] (:call-stack-info @mi)))
+
+(defn call-stack-str []
+  (->> (call-stack-info) (interpose '->) vec (str "  ===  current call stack: "))
+  )
+
+(defn append-call-stack! [m & ms]
+  (swap! (mon-info) #(apply update-in % [:call-stack-info] conj m ms)))
+
+(defn reset-call-stack! [st]
+  (swap! (mon-info) assoc-in [:call-stack-info] st))
+
+(defn top-time-monitor-existed? []
+  (:top-time-monitor-existed? @(mon-info)))
+
+(defn set-top-time-monitor! []
+  (swap! (mon-info) assoc :top-time-monitor-existed? true))
+
+(defn initial-mon-info [stack-info]
+  (atom {:call-stack-info [stack-info]}))
+
+(defmacro begin-stack [stack-info & body]
+  `(with-mon-info (initial-mon-info ~stack-info) ~@body)
+  )
+
+(defmacro enter-stack [stack-info & body]
+  `(do (append-call-stack! ~stack-info) ~@body))
+
+(defmacro enter-mon-time [stack-info & body]
+  (let [body `(let [start# (System/nanoTime)
+                    i# (count (call-stack-info))
+                    ret# (enter-stack ~stack-info ~@body) 
+                    ]
+                (let [end# (System/nanoTime)
+                      elapsed# (-> (/ (double (- end# start#)) 1000000.0) (str " ms"))
+                      st-info# (call-stack-info)
+                      [first-half# second-half#] (split-at i# st-info#)
+                      st-info# (coll/concatv first-half# [{~stack-info elapsed#}] (rest second-half#))
+                      ]
+                  (reset-call-stack! st-info#)
+                  ret#))
+        ]
+    `(if-let [top-time-monitor-existed?# (top-time-monitor-existed?)]
+             ~body
+             (do
+               (set-top-time-monitor!)
+               (let [r# ~body]
+                 (log/info (call-stack-str))
+                 r#)))))
+
+(defmacro begin-mon-time [stack-info & body]
+  `(with-mon-info (atom {:call-stack-info []}) (enter-mon-time ~stack-info ~@body))
+  )
+
+(defmacro start-monitor [mon-type stack-info & body]
+  (if (= mon-type :time)
+    `(begin-mon-time ~stack-info ~@body)
+    `(begin-stack ~stack-info ~@body)))
+
+(defmacro join-monitor [mon-type stack-info & body]
+  (if (= mon-type :time)
+    `(enter-mon-time ~stack-info ~@body)
+    `(enter-stack ~stack-info ~@body)))
+
+(defmacro monitor [mon-type stack-info & body]
+  `(if (mon-info)
+     (join-monitor ~mon-type ~stack-info ~@body)
+     (start-monitor ~mon-type ~stack-info ~@body)))
+
+(defmacro monitor-exec-state
+  "
+  Usually you only need to call this macro. The other macros above don't tend to be called directly by programers.
+  "
+  [ns-sym fn-sym args & body]
+  (if-let [mon-type (get-monitor-cfg ns-sym fn-sym)]
+          `(monitor ~mon-type (apply list (sym/str-symbol '~ns-sym "/" '~fn-sym) ~args) ~@body)
+          `(do ~@body)))
---
+++ src/clj/ladybird/svc/generate.clj
@@ -0,0 +1,24 @@
+(ns ladybird.svc.generate
+    (:use [ladybird.util.core :only (get-stack-trace-str)])
+    (:require [ladybird.misc.monitor :as mon]
+              [clojure.tools.logging :as log])
+    )
+
+(defn with-stack [{:keys [svc prototype body-form] :as meta}]
+  (let [svc-info `(apply list '~svc ~prototype)
+        body-form `(mon/monitor-exec-state ~(ns-name *ns*) ~svc ~prototype ~body-form)
+        ]
+    (assoc meta :body-form body-form)))
+
+(defn stack-error-str [e]
+  (clojure.string/join "\n" [(type e)
+                             (mon/call-stack-str)
+                             (get-stack-trace-str e)]))
+
+(defn catch-stack [{:keys [body-form] :as meta}]
+  (let [catch-forms `((catch Exception e#
+                             (log/error (stack-error-str e#))
+                             (ex-info "Oops!" {:ex-type :other-exception})))
+        body-form `(try ~body-form ~@catch-forms)
+        ]
+    (assoc meta :body-form body-form)))
