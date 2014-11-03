5aa67f7ef6d6cebbc78e51516b9ea220ca8ab5e1
xzj <xzj007@gmail.com>
2014-11-03 17:15:12 +0800

    Changed: fixed the bug when multiple child monitors existed in the call path

--- src/clj/ladybird/misc/monitor.clj
+++ src/clj/ladybird/misc/monitor.clj
@@ -5,7 +5,7 @@
               [ladybird.util.coll :as coll]
               [clojure.tools.logging :as log]))
 
-;; We usually need to monitor the execution of our programs at some time. Normally we accomplish this task with logging(or something similar, like pritnln etc.) directly. But the model of logging doesn't match monitoring perfectly. The various levels of logging(trace, debug, info, warn, error) don't have so much business with monitoring. And the logging statements spreading in our codes make them in a mess.
+;; As a developer, we usually need to monitor the execution of our programs at some time. Normally we accomplish this task with logging(or something similar, like pritnln etc.) directly. But the model of logging doesn't match monitoring perfectly. The various levels of logging(trace, debug, info, warn, error) don't have so much business with monitoring. And the logging statements spreading in our codes make them in a mess.
 ;;
 ;; So let's think about what we need when we monitor. The one of the two things we most likely need when monitoring is to monitor the execution path of the code and the arguments' value passed to the functions on the path when errors occured. The other thing is to monitor the elapsed time of some portions of the execution path when performance problems occured.
 ;;
@@ -26,7 +26,6 @@
   "
   ([ns-sym val] {:pre [(contains? #{:time :exec-path false} val)]} 
    (swap! monitor-cfg assoc ns-sym val))
-  ;; TODO: change key from vector to qualified symbol
   ([ns-sym var-sym val] {:pre [(contains? #{:time :exec-path false} val)]} 
    (swap! monitor-cfg assoc (sym/str-symbol ns-sym "/" var-sym) val)))
 
@@ -42,34 +41,124 @@
 ;; monitor infrastructure
 (def-bindable mon-info nil)
 
+(defn assoc-in-mon-info! [ks v]
+  (swap! (mon-info) assoc-in ks v))
+
+(defn assoc-mon-info! [k v]
+  (assoc-in-mon-info! [k] v))
+
+(defn get-in-mon-info [& ks]
+  (when-let [mi (mon-info)] (get-in @mi ks)))
+
 (defn call-stack-info []
-  (when-let [mi (mon-info)] (:call-stack-info @mi)))
+  (get-in-mon-info :call-stack-info))
+
+(defn stack-at [indices]
+  (get-in (call-stack-info) indices))
+
+(defn v-stack
+  "
+  Constructs a vector representing a call path.
+  "
+  [& sts]
+  (apply vector ::v sts))
+
+(defn h-stack
+  "
+  Constructs a vector representing serial function calls of the same level in the call path.
+  "
+  [& sts]
+  (apply vector ::h sts))
+
+(defn- first-index [indices]
+  (assoc indices (dec (count indices)) 0))
+
+(defn- first-element [indices]
+  (-> indices first-index stack-at))
+
+(defn in-v-stack? [indices]
+  (= ::v (first-element indices)))
+
+(defn in-h-stack? [indices]
+  (= ::h (first-element indices)))
+
+(defn v-stack? [st]
+  (and (vector? st) (= ::v (first st))))
+
+(defn h-stack? [st]
+  (and (vector? st) (= ::h (first st))))
 
 (defn call-stack-str []
-  (->> (call-stack-info) (interpose '->) vec (str "  ===  current call stack: "))
-  )
+  (let [call-stack (call-stack-info)
+        f #(cond
+             (v-stack? %) (->> (rest %) (interpose '->) vec)
+             (h-stack? %) (->> (rest %) (interpose '|) )
+             :others %)
+        call-stack (clojure.walk/postwalk f call-stack)
+        ]
+    (str "  ===  current call stack: " call-stack)))
 
-(defn append-call-stack! [m & ms]
-  (swap! (mon-info) #(apply update-in % [:call-stack-info] conj m ms)))
+(defn inc-last-index [indices]
+  (->> (last indices) inc (assoc indices (dec (count indices)))))
 
-(defn reset-call-stack! [st]
-  (swap! (mon-info) assoc-in [:call-stack-info] st))
+(defn assoc-in-stack! [is st]
+  (assoc-in-mon-info! (apply conj [:call-stack-info] is) st))
 
-(defn replace-a-stack! [i st]
-  (let [st-info (call-stack-info)
-        [first-half second-half] (split-at i st-info)
-        st-info (coll/concatv first-half [st] (rest second-half))
-        ] 
-    (reset-call-stack! st-info)))
+(defn set-call-stack! [st]
+  (assoc-in-mon-info! [:call-stack-info] st))
+
+(defn- adjust-v-stack [parent-indices]
+  (let [info (call-stack-info)
+        parent-container-indices (drop-last parent-indices)
+        parent-container (get-in info parent-container-indices)
+        parent-index (last parent-indices)
+        [first-part second-part] (split-at (inc parent-index) parent-container)
+        first-part (vec first-part)
+        second-part (if (= 1 (count second-part)) (first second-part) (apply v-stack second-part))
+        second-part (h-stack second-part)
+        parent-container (conj first-part second-part)
+        ]
+    (if (empty? parent-container-indices) parent-container (assoc-in info parent-container-indices parent-container))))
+
+(defn- adjust-parent-child-indices! [parent-indices]
+  (if (in-v-stack? parent-indices)
+    (let [is (inc-last-index parent-indices)
+          st (stack-at is)
+          is (if st
+               (if (h-stack? st)
+                 (->> st count (conj is))
+                 (do
+                   (set-call-stack! (adjust-v-stack parent-indices))
+                   (conj is 2)))
+               is)
+          ]
+      [parent-indices is])
+    (do
+      (assoc-in-stack! parent-indices (v-stack (stack-at parent-indices)))
+      [(conj parent-indices 1) (conj parent-indices 2)])))
+
+(defn parent-stack-indices []
+  (get-in-mon-info :parent-stack-indices))
+
+(defn set-parent-stack-indices! [indices]
+  (assoc-mon-info! :parent-stack-indices indices))
+
+(defn append-stack! [m]
+  (let [parent-indices (parent-stack-indices)
+        [parent-indices current-indices] (adjust-parent-child-indices! parent-indices)
+        ]
+    (assoc-in-stack! current-indices m)
+    (set-parent-stack-indices! current-indices)
+    parent-indices))
 
 (defn top-time-monitor-existed? []
   (:top-time-monitor-existed? @(mon-info)))
 
-(defn- assoc-mon-info! [k v]
-       (swap! (mon-info) assoc k v))
-
 (defn set-top-time-monitor! []
   (assoc-mon-info! :top-time-monitor-existed? true))
+
+(defn clean-top-time-monitor! []
+  (swap! (mon-info) dissoc :top-time-monitor-existed?))
 
 (defn set-error-occured! []
   (assoc-mon-info! :error-occured? true))
@@ -78,43 +167,49 @@
   (:error-occured? @(mon-info)))
 
 (defn initial-mon-info [stack-info]
-  (atom {:call-stack-info [stack-info]}))
+  (atom {:call-stack-info (v-stack stack-info) :parent-stack-indices [1]}))
 
 ;; monitor functionality 
 (defmacro begin-stack [stack-info & body]
-  `(with-mon-info (initial-mon-info ~stack-info) ~@body)
-  )
+  `(with-mon-info (initial-mon-info ~stack-info) ~@body))
 
 (defmacro enter-stack [tag-completed? stack-info & body]
-  (let [body `(do (append-call-stack! ~stack-info) ~@body)]
-    (if tag-completed?
-      `(let [i# (count (call-stack-info))
-             r# ~body]
-         (replace-a-stack! i# {~stack-info :completed})
-         r#)
-      body)))
+  (let [complete-body (when tag-completed?
+                        `(assoc-in-stack! (parent-stack-indices) {~stack-info :completed})
+                        )
+        body `(let [parent-indices# (append-stack! ~stack-info)
+                    r# (do ~@body)
+                    ]
+                ~complete-body
+                (set-parent-stack-indices! parent-indices#)
+                r#
+                )]
+    body))
 
 (defmacro enter-mon-time [stack-info & body]
   (let [body `(let [start# (System/nanoTime)
-                    i# (count (call-stack-info))
-                    ret# (enter-stack false ~stack-info ~@body) 
+                    parent-indices# (append-stack! ~stack-info)
+                    ret# (do ~@body)
+                    current-indices# (parent-stack-indices)
                     ]
                 (let [end# (System/nanoTime)
                       elapsed# (-> (/ (double (- end# start#)) 1000000.0) (str " ms"))
                       ]
-                  (replace-a-stack! i# {~stack-info elapsed#})
-                  ret#))
-        ]
+                  (assoc-in-stack! current-indices# {~stack-info elapsed#})
+                  (set-parent-stack-indices! parent-indices#)
+                  ret#)
+                )]
     `(if-let [top-time-monitor-existed?# (top-time-monitor-existed?)]
              ~body
              (do
                (set-top-time-monitor!)
                (let [r# ~body]
                  (when-not (error-occured?) (log/info (call-stack-str)))
+                 (clean-top-time-monitor!)
                  r#)))))
 
 (defmacro begin-mon-time [stack-info & body]
-  `(with-mon-info (atom {:call-stack-info []}) (enter-mon-time ~stack-info ~@body))
+  `(with-mon-info (atom {:call-stack-info (v-stack) :parent-stack-indices [0]}) (enter-mon-time ~stack-info ~@body))
   )
 
 (defmacro start-monitor [mon-type stack-info & body]
