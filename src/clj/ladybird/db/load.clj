0a134ce35c07e24f9b1ec846d3a939dea487f302
xzj <xzj007@gmail.com>
2015-12-02 19:41:52 +0800

    Changed: doc string.
    	modified:   src/clj/ladybird/db/load.clj

--- src/clj/ladybird/db/load.clj
+++ src/clj/ladybird/db/load.clj
@@ -36,8 +36,8 @@
             :host \"localhost\"
             :port 3306
             :db \"cookbook\"
-            :user \"xzj\"
-            :password \"xzjamjzx\"
+            :user \"username\"
+            :password \"password\"
            }
        :main {.......}
       }
