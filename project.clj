6a57320ffbef9e77be07e67c6078781ebd950d08
xzj <xzj007@gmail.com>
2020-06-28 20:16:20 +0800

    Upgraded version and dev dependency. See #3.

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
-(defproject coders-at-work/ladybird "0.5.6-SNAPSHOT"
+(defproject coders-at-work/ladybird "0.6.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.logging "0.3.1"]
+  :dependencies [[org.clojure/tools.logging "0.3.1"]
                  [korma "0.4.2"]
                  ]
   :profiles {:dev {:dependencies [[mysql/mysql-connector-java "5.1.25"]
-                                  [midje "1.8.2"]
-                                  [midje-notifier "0.2.0"]
+                                  [midje "1.9.9"]
+                                  [midje-notifier "0.3.0"]
+                                  [org.clojure/clojure "1.9.0"]
                                   ]}}
   :source-paths ["src/clj"]
   :java-source-paths ["src/java"]
--- test/ladybird/util/t_core.clj
+++ test/ladybird/util/t_core.clj
@@ -6,7 +6,7 @@
 
 (facts "Can construct a string containing the class name, message, exception data and stack trace of an exception"
        (fact "Can construct the string if exception data exists"
-             (get-stack-trace-str (ex-info "a" {:a 1})) => #"clojure.lang.ExceptionInfo: a \{:a 1\}\nclojure\.core\$ex_info\.invoke\(core\.clj:\d+\)\n.+"
+             (get-stack-trace-str (ex-info "a" {:a 1})) => #"clojure.lang.ExceptionInfo: a \{:a 1\}\n.+"
              )
        (fact "Can construct the string if exception data doesn't exist"
              (get-stack-trace-str (Exception. "a")) => #"java.lang.Exception: a\n.+"
