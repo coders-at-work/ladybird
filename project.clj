06de61bcc5af7eb652423e9fe05d53c57750371a
Eric Fang <gzeureka@gmail.com>
2021-01-07 20:44:31 +0800

    修改version 为 0.6.3-SNAPSHOT
    之前错用 lein release 将 version 改为了 0.6.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject coders-at-work/ladybird "0.6.3"
+(defproject coders-at-work/ladybird "0.6.3-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
